a51688067c6dae2362d7debd2d08a003a339fddd
mdsnazir <36910794+mdsnazir@users.noreply.github.com>
2020-04-29 15:34:55 -0400

    Update makeBmatrix.m

--- makeBmatrix.m
+++ makeBmatrix.m
@@ -1,14 +1,20 @@
+% Copywright: Md Salman Nazir
+% 01/29/2020
 
 function B = makeBmatrix(Nb,unclrbin)
-B = zeros(2*Nb,2*Nb);
-for i=1:unclrbin
-    B(i,i)=1;
-    B(i,2*Nb-i+1)=1;
-end
-for i=Nb+1:(2*Nb-unclrbin)
-    B(i,i)=1;
-    B(i,2*Nb-i+1)=1;
-end
-% test: print B
-B;
+% Control - B-matrix for priority- or market-based on/off switching of TCLs
+% Nb: Number of bins representing TCL on/off states
+% unclrbin: Bin index up to which all TCLs must turn off. Above it, all TCLs must turn on.
+
+    B = zeros(2*Nb,2*Nb);
+    for i=1:unclrbin
+        B(i,i)=1;
+        B(i,2*Nb-i+1)=1;
+    end
+    for i=Nb+1:(2*Nb-unclrbin)
+        B(i,i)=1;
+        B(i,2*Nb-i+1)=1;
+    end
+    % test: print B
+    B;
 end
