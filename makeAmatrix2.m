20793bb555289588865b41300af82a58dd08b976
mdsnazir <36910794+mdsnazir@users.noreply.github.com>
2020-05-11 23:33:52 -0400

    Update makeAmatrix2.m

--- makeAmatrix2.m
+++ makeAmatrix2.m
@@ -2,6 +2,14 @@
 % This software is distributed under the 3-clause BSD License.
 
 function A = makeAmatrix2(Nb,a0,a1)
+
+% Build a Markov-transition matrix from TCL parameters (cooling) (i.e. air-conditioner (AC))
+% Inputs: 
+% a0: rate of temperature increase due to ambient when AC is off
+% a1: rate of temperature decrease due to AC on
+% Nb: number of discrete bins in either on/off states. 
+% Outputs:
+% A: Transition matrix. Gives the aggregate TCL dynamics as: Xdot = A X (an LTI system)
     A = zeros(2*Nb);
     for i = 2:Nb
         A(i,i-1) = a0;
